14ea82ac23c5e8a16d49f55baa745eafffe2dbdf
Md-Nrahman <48413605+Md-Nrahman@users.noreply.github.com>
2020-04-21 09:45:25 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function initMap() {
+       const loc ={lat:23.785590, lng:90.385949};
+
+       const map= new google.maps.Map(document.querySelector('.map')
+       	,{
+       		zoom: 14,
+       		center:loc
+       	});
+
+       const marker = new google.maps.Marker({position: loc, map:map});
+      }
+
+
+
+
+window.addEventListener('scroll', function(){
+	if (window.scrollY>150) {
+		document.querySelector('#navbar').style.opacity=0.9;
+
+	} else {
+		document.querySelector('#navbar').style.opacity=1;
+	}
+});
+
+
+
+
+
+      $('#navbar a, .btn').on('click', function(event){
+      	if(this.hash !==''){
+      		event.preventDefault();
+
+      		const hash= this.hash;
+      		$('html, body').animate(
+      		{
+      			scrollTop: $(hash).offset().top-100
+      		},
+      		800
+      		);
+      	}
+      });
